538c187583a27cdc61ee94c003ecb5dd8670600e
Triloki Sharma <156106678+Trilokisharma19@users.noreply.github.com>
2024-09-14 07:05:14 -0700

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+String = "";
+let arr = Array.from(buttons);
+
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      String = eval(String);
+      input.value = String;
+    } else if (e.target.innerHTML == "AC") {
+      String = "";
+      input.value = String;
+    } else if (e.target.innerHTML == "DEL") {
+      String = String.substring(0, String.length - 1);
+      input.value = String;
+    } else {
+      String += e.target.innerHTML;
+      input.value = String;
+    }
+  });
+});
